0e34d864ba0509bc511763331a479b74ac99d60c
Trystan Roches <trystan.rcs4@gmail.com>
2022-02-03 20:52:13 +0100

    feat: add color 3d plot & save figures

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from src.wadge_predict import ScoringRecipes
 data_name = ["Person","Recipe", "Score"]
 data = [(1, 1, 5), (1, 2, 1),(2, 1, 1), (2, 2, 5), (3, 1, 3), (3, 2, 3), (4, 1, 5), (4, 2, 1),(5, 1, 1), (5, 2, 5), (6, 1, 3), (6, 2, 3), (7, 1, 5), (7, 2, 1),(8, 1, 1), (8, 2, 5), (9, 1, 3), (9, 2, 3), (10, 1, 5), (10, 2, 1),(11, 1, 1), (11, 2, 5), (12, 1, 3), (12, 2, 3)]
 
-# les labels associés aux enregistrements Clustering par notes -> positif = 0 / négatif = 1
+# les labels associés aux enregistrements Clustering par notes -> positif = 0 // négatif = 1 // entre min et max/2 = 2 // entre max/2 et max = 3
 # on recupere la note et on l'associe a un cluster 
 
 le_max = -1000
@@ -27,7 +27,7 @@ for triplet in data:
         target.append(3)
 
 model = ScoringRecipes(data=data, target=target, data_name=data_name)
-model._make_ml_modele(2)
-model._res_model()
+model._make_knn(2)
+model._res_knn()
 model._print_score_matrice()
 model._print_knn_diagram()
--- src/wadge_predict.py
+++ src/wadge_predict.py
@@ -8,42 +8,39 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import pandas as pd
 
-
 class ScoringRecipes:
     def __init__(self, data: list, target: dict, data_name: list):
         self.data = data
         self.target = target
         self.data_name = data_name
-        self.recipes_profil = []
+        self.recipes_knn = []
         self.data_train = []
         self.data_test = []
         self.target_train = []
         self.target_test = []
-        self.result = []
+        self.result_knn = []
         self.matrice = []
         self.accuracy = 0
         self.faux_neg = 0
         self.faux_pos = 0
         self.cross_knn = []
 
-
-    def _make_ml_modele(self, nb_knn: int):
+    def _make_knn(self, nb_knn: int):
         data_frame = pd.DataFrame(self.data)
         self.data_train, self.data_test, self.target_train, self.target_test = train_test_split(data_frame, self.target, random_state=0, train_size=0.5)
 
         # KNN Model -> k neighbors
-        self.recipes_profil = neighbors.KNeighborsClassifier(n_neighbors= nb_knn)
+        self.recipes_knn = neighbors.KNeighborsClassifier(n_neighbors= nb_knn)
 
-        self.recipes_profil.fit(self.data_train, self.target_train)
+        self.recipes_knn.fit(self.data_train, self.target_train)
 
-
-    def _res_model(self):
+    def _res_knn(self):
         
         # résultat de la prédiction 
-        self.result = self.recipes_profil.predict(self.data_test)
+        self.result_knn = self.recipes_knn.predict(self.data_test)
 
         # cross validation
-        self.cross_knn = cross_val_score(self.recipes_profil, self.data, self.target )
+        self.cross_knn = cross_val_score(self.recipes_knn, self.data, self.target )
 
         # matrice de confusion
         self.matrice = confusion_matrix(self.result, self.target_test)
@@ -62,39 +59,36 @@ class ScoringRecipes:
         sns.heatmap(self.matrice, square=True, annot=True, cbar=False)
         plt.xlabel('valeurs prédites')
         plt.ylabel('valeurs réelles')
-        plt.show()
+        plt.savefig("conf_matrice.png")
 
     def _print_knn_diagram(self):
         fig = plt.figure()
         ax = fig.add_subplot(projection='3d')
 
-        # For each set of style and range settings, plot n random points in the box
-        # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
-        # numb = random(0,24)
         color = []
-        # for _ in self.data :
-        #     if self.target == 0 :
-        #         color.append("#000000")
-        #     if self.target == 1 :
-        #         color.append("#FFFFFF")
-        #     if self.target == 2 :
-        #         color.append("#282222")
-        #     if self.target == 3 :
-        #         color.append("ABABAB")
-        # print(len(color))
+        for _ in self.target :
+            if _ == 0 :
+                color.append("#3DE70A") # vert = score très élevé
+            if _ == 1 :
+                color.append("#FF0000") # rouge = score très bas
+            if _ == 2 :
+                color.append("#FCFF00") # orange = score en dessous de la moyenne
+            if _ == 3 :
+                color.append("#B4C802") # jaune = score au dessus de la moyenne
+        print(len(color))
 
         ax.scatter([triplet_score[2] for triplet_score in self.data], [triplet_id_person[0] for triplet_id_person in self.data], [triplet_recipe[1] for triplet_recipe in self.data], c=color)
 
         ax.set_xlabel('Score')
         ax.set_ylabel('Person')
         ax.set_zlabel('Recipes')
+        plt.grid()
+        plt.savefig("3d_dots_diagram")
 
-        plt.show()
 
 
+# TODO -> train / allez chercher les "points autour" de l'utilisateur
+# id_user = 1 -> trouver les recettes des users du même profil avec un score élevé (renvoyer les id_recipes)
+# Ensuite prédire sur toutes les recettes en fonction de "? = ingrédients, notes, durée ..."
 
-# TODO -> train / allez chercher les "points autour" de l'utilisateur /
-
-# TODO -> récup les recettes K-means et ensuite prédire sur toutes les recettes en fonction de "? = ingrédients, notes, durée ..."
-
-# k-means ou DecisionTree? lequel est le plus accurate
+# TODO -> k-means ou DecisionTree? lequel est le plus accurate
