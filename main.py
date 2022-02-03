fcff16843387b7abfc74bcd2f883cb50051e1f13
Trystan Roches <trystan.rcs4@gmail.com>
2022-02-03 15:45:22 +0100

    feat: make generic cluster & add cross validation

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
+from math import sqrt
 from src.wadge_predict import ScoringRecipes
 
 data_name = ["Person","Recipe", "Score"]
 data = [(1, 1, 5), (1, 2, 1),(2, 1, 1), (2, 2, 5), (3, 1, 3), (3, 2, 3), (4, 1, 5), (4, 2, 1),(5, 1, 1), (5, 2, 5), (6, 1, 3), (6, 2, 3), (7, 1, 5), (7, 2, 1),(8, 1, 1), (8, 2, 5), (9, 1, 3), (9, 2, 3), (10, 1, 5), (10, 2, 1),(11, 1, 1), (11, 2, 5), (12, 1, 3), (12, 2, 3)]
 
-# les labels associés aux enregistrements Clustering par notes -> 1 à 2 = 0 , 3 = 1, 4 à 5 = 2
-#on recupere la note et on l'associe a un cluster 
+# les labels associés aux enregistrements Clustering par notes -> positif = 0 / négatif = 1
+# on recupere la note et on l'associe a un cluster 
+
+le_max = -1000
+le_min = 1000
+
+for triplet in data:
+    le_max = max(le_max, triplet[2])
+    le_min = min(le_min, triplet[2])
+
+
 target = []
 for triplet in data:
-    if(triplet[2] >= 4 ):
-        target.append(2)
-    elif(triplet[2] <= 2 ):
+    if triplet[2] == le_max :
         target.append(0)
-    elif(triplet[2] == 3):
+    elif triplet[2] == le_min :
         target.append(1)
+    elif triplet[2] > le_min and triplet[2] < sqrt(le_max):
+        target.append(2)
+    elif triplet[2] > sqrt(le_max) and triplet[2] < le_max :
+        target.append(3)
 
 model = ScoringRecipes(data=data, target=target, data_name=data_name)
 model._make_ml_modele(2)
 model._res_model()
 model._print_score_matrice()
+model._print_knn_diagram()
--- src/wadge_predict.py
+++ src/wadge_predict.py
@@ -1,7 +1,9 @@
-import numbers
+from random import random
+from numpy import append
 from sklearn import neighbors
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 import seaborn as sns
 from matplotlib import pyplot as plt
 import pandas as pd
@@ -22,7 +24,7 @@ class ScoringRecipes:
         self.accuracy = 0
         self.faux_neg = 0
         self.faux_pos = 0
-        
+        self.cross_knn = []
 
 
     def _make_ml_modele(self, nb_knn: int):
@@ -40,6 +42,9 @@ class ScoringRecipes:
         # résultat de la prédiction 
         self.result = self.recipes_profil.predict(self.data_test)
 
+        # cross validation
+        self.cross_knn = cross_val_score(self.recipes_profil, self.data, self.target )
+
         # matrice de confusion
         self.matrice = confusion_matrix(self.result, self.target_test)
 
@@ -51,13 +56,44 @@ class ScoringRecipes:
     def _print_score_matrice(self):
 
         print("Résultat : ", self.result)
-        print("Accuracy : " + str(self.accuracy) + "\n" + "TPR : " + str(self.faux_pos) + "\n" + "FPR : " + str(self.faux_neg))
+        print("Cross Validation : ", self.cross_knn)
 
+        print("Accuracy : " + str(self.accuracy) + "\n" + "TPR : " + str(self.faux_pos) + "\n" + "FPR : " + str(self.faux_neg))
         sns.heatmap(self.matrice, square=True, annot=True, cbar=False)
         plt.xlabel('valeurs prédites')
         plt.ylabel('valeurs réelles')
         plt.show()
 
+    def _print_knn_diagram(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+
+        # For each set of style and range settings, plot n random points in the box
+        # defined by x in [23, 32], y in [0, 100], z in [zlow, zhigh].
+        # numb = random(0,24)
+        color = []
+        # for _ in self.data :
+        #     if self.target == 0 :
+        #         color.append("#000000")
+        #     if self.target == 1 :
+        #         color.append("#FFFFFF")
+        #     if self.target == 2 :
+        #         color.append("#282222")
+        #     if self.target == 3 :
+        #         color.append("ABABAB")
+        # print(len(color))
+
+        ax.scatter([triplet_score[2] for triplet_score in self.data], [triplet_id_person[0] for triplet_id_person in self.data], [triplet_recipe[1] for triplet_recipe in self.data], c=color)
+
+        ax.set_xlabel('Score')
+        ax.set_ylabel('Person')
+        ax.set_zlabel('Recipes')
+
+        plt.show()
+
+
+
+# TODO -> train / allez chercher les "points autour" de l'utilisateur /
 
 # TODO -> récup les recettes K-means et ensuite prédire sur toutes les recettes en fonction de "? = ingrédients, notes, durée ..."
 
