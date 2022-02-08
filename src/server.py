554d175233418d31889ad28b90daacada57de215
Romain Vacheret <romain.vacheret@gmail.com>
2022-02-08 07:04:47 +0100

    feat: filter suggested recipes

--- src/server.py
+++ src/server.py
@@ -1,5 +1,8 @@
 from typing import Tuple
+from itertools import chain
+
 from flask import Flask, request, jsonify
+
 from kmeans import compute_kmeans # TODO change path
 
 app = Flask('wadge-ml')
@@ -11,7 +14,7 @@ def transform_recipes(user: dict) -> Tuple[list[int], list[Tuple[int, int]]]:
 	recipes_score = []
 
 	from pprint import pprint
-	pprint(user)
+	# pprint(user)
 
 	for recipe in user['recipes']:
 		users_ids.append(user['id'])
@@ -24,6 +27,7 @@ def transform_recipes(user: dict) -> Tuple[list[int], list[Tuple[int, int]]]:
 	# 	for recipe in user['recipes']]
 
 	# return [user_id] * len(recipes), recipes
+
 
 def regroup(users: list[dict]):
 	users_ids = []
@@ -38,6 +42,43 @@ def regroup(users: list[dict]):
 
 	return users_ids, recipes_ids, recipes_score
 
+def get_user1_length(users: list[dict]) -> int:
+	return [len(user['recipes']) for user in users if user['name'] == '1'][0]
+
+def get_user1_id(users: list[dict]) -> int:
+	return [user['id'] for user in users if user['name'] == '1'][0]
+
+def to_tuple(user: dict) -> Tuple[int, int, int]:
+	return [(user['id'], recipe['recipe']['id'], recipe['score']) \
+		for recipe in user['recipes']]
+
+def transform(users: list[dict]) -> list[Tuple[int, int, int]]:
+	return list(chain(*map(to_tuple, users)))
+
+
+def foo(tuples: list[Tuple[int, int, int]], 
+	length: int, 
+	id: int, clusters: list[int]):
+
+	user1_recipes = [t for t in tuples if t[0] == id]
+	user1_recipes_ids = [t[1] for t in user1_recipes] 
+	top_10 = sorted(user1_recipes, key= lambda x: x[-1])[:10] # Must be sure there are at least 10
+	print(top_10)
+	top_10_idx = [tuples.index(t) for t in top_10]
+	top_10_clusters = [clusters[idx] for idx in top_10_idx]
+	print(top_10_clusters)
+	return foo2(tuples, clusters, set(top_10_clusters)) - set(user1_recipes_ids)
+	# top_10_ids = [t[1] for t in top_10]
+
+def foo2(tuples, clusters, clusters_ids) -> list[int]:
+	return set(t[1] for t, c  in zip(tuples, clusters) if c in clusters_ids)
+
+class O:
+	def __init__(self, u_id, r_id, score, label_=None) :
+		self.u_id = u_id 
+		self.r_id = r_id
+		self.score = score
+		self.label_ = label_
 
 @app.route('/knn', methods=['POST'])
 def compute_knn():
@@ -52,15 +93,28 @@ def compute_knn():
 		# # print(transformed_recipes)
 		# print(b)
 		# # TODO use KNN and return result
-		from pprint import pprint
+		# from pprint import pprint
 		users_ids, recipes_ids, scores = regroup(users)
-		print(len(users_ids))
-		print(len(scores))
+		# print(len(users_ids))
+		# print(len(scores))
 		x = compute_kmeans([[score] for score in scores])
-		print(x)
-		print(len(x))
+		# print(x)
+		# print(len(x))
+		# print(transform(users))
+		print(sum(len(user['recipes']) for user in users))
+		user1_recipe_count = get_user1_length(users)
+		user1_id = get_user1_id(users)
+		result = foo(
+			transform(users),
+			user1_recipe_count,
+			user1_id,
+			x
+		)
 
-		return jsonify([])
+		print(result)
+		# print(user1_id)
+
+		return jsonify(list(result))
 
 
 if __name__ == '__main__':
